ad7aad9655c32ef275cc352a789e1d493b547a03
Ginnian <virginia.waikari@gmail.com>
2019-11-01 01:37:39 +1300

    Added validation data

--- Videofy/Models/Movie.cs
+++ Videofy/Models/Movie.cs
@@ -9,16 +9,25 @@ namespace Videofy.Models
 {
     public class Movie
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
+        public int Id { get; set; } //Primary key
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
-        public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
+        [StringLength(60, MinimumLength = 3)] //Maximum char 3 < x < 60
+        [Required]              //Field must have a value
+        public string Title { get; set; } //Title of movie.
 
-        [Column(TypeName = "decimal(18, 2)")]
-        public decimal Price { get; set; }
+        [Display(Name = "Release Date")] //Format name that appears as a label
+        [DataType(DataType.Date)]       //Check input is type date.
+        public DateTime ReleaseDate { get; set; } //Set or get the release date.
+
+        [Range(1, 100)] //price must be between $1 and $100
+        [DataType(DataType.Currency)] //Format input to currency data type
+        [Column(TypeName = "decimal(18, 2)")] //Round decimal to 2 places
+        public decimal Price { get; set; } //Get or set price of movie
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")] //Prevent unvalidated characters
+        [Required]  //Field must have a genre
+        [StringLength(30)] //Genre must be maximum 30 characters
+        public string Genre { get; set; } //Get or set movie genre
     }
 
 }
