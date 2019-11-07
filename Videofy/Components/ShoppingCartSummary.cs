906d2e625d3c563996f9efa2480bba3fac1685da
Paolo-Man001 <p.a.2manlunas@gmail.com>
2019-11-07 23:51:13 +1300

    Add Component folders in Project folder and View/Shared folder to keep ShoppingCartSummary.cs and View.

---
+++ Videofy/Components/ShoppingCartSummary.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Videofy.Models;
+
+namespace Videofy.Components
+{
+    public class ShoppingCartSummary:ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+
+        public IViewComponentResult Invoke()
+        {
+            var items = new List<ShoppingCartItem>() { new ShoppingCartItem(), new ShoppingCartItem() }; // Dummy items For TEST
+            //var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+            return View(shoppingCartViewModel);
+        }
+    }
+}
