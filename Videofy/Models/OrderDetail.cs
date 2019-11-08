cf4fb719b4c3c1fbecd6f13339b787abf703cca1
Paolo-Man001 <p.a.2manlunas@gmail.com>
2019-11-08 15:21:11 +1300

    Add Order and Order details model. Create IOrderInterface and OrderRepository to inject orders and detail into OrderDetail db.

---
+++ Videofy/Models/OrderDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Videofy.Models
+{
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+        public int MovieId { get; set; }
+
+        [DataType(DataType.Currency)] //Format input to currency data type
+        [Column(TypeName = "decimal(18, 2)")] //Round decimal to 2 places
+        public decimal Price { get; set; }
+        public virtual Movie Movie { get; set; }
+        public virtual Order Order { get; set; }
+    }
+}
---
+++ Videofy/Models/Repositories/OrderRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Videofy.Data;
+using Videofy.Models.Interface;
+
+
+/// <summary>
+/// This will be used to inject the order item and its detail from  sql databse
+/// into the OrderDetails sql db.
+/// </summary>
+namespace Videofy.Models.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly MvcMovieContext _mvcMovieContext;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderRepository(MvcMovieContext mvcMovieContext, ShoppingCart shoppingCart)
+        {
+            _mvcMovieContext = mvcMovieContext;
+            _shoppingCart = shoppingCart;
+        }
+
+        public void CreateOrder(Order order)
+        {
+            _mvcMovieContext.Orders.Add(order);
+
+            var shoppingCartItems = _shoppingCart.ShoppingCartItems;
+
+            foreach (var item in shoppingCartItems)
+            {
+                var orderDetail = new OrderDetail()
+                {
+                    MovieId = item.Movie.Id,
+                    OrderId = order.OrderId,
+                    Price = item.Movie.Price,
+                };
+
+                _mvcMovieContext.OrderDetails.Add(orderDetail);
+            }
+            _mvcMovieContext.SaveChanges();
+        }
+    }
+}
+
+//public int OrderDetailId { get; set; }
+//public int OrderId { get; set; }
+//public int MovieId { get; set; }
+
+//[DataType(DataType.Currency)] //Format input to currency data type
+//[Column(TypeName = "decimal(18, 2)")] //Round decimal to 2 places
+//public decimal Price { get; set; }
+//public virtual Movie Movie { get; set; }
+//public virtual Order Order { get; set; }
