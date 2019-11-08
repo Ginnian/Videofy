39efe51e8bdcd9d40e60093e4f2f47dbb0fa577b
Paolo-Man001 <p.a.2manlunas@gmail.com>
2019-11-08 18:41:11 +1300

    Add more properties in Order.cs, Add-Migration and UpdateDb for Order.cs

---
+++ Videofy/Migrations/20191108053745_AddOrderProperties.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Videofy.Migrations
+{
+    public partial class AddOrderProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Orders",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Orders",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Orders",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "OrderPlaced",
+                table: "Orders",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "OrderTotal",
+                table: "Orders",
+                type: "decimal(18, 2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "OrderPlaced",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "OrderTotal",
+                table: "Orders");
+        }
+    }
+}
--- Videofy/Models/Order.cs
+++ Videofy/Models/Order.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +10,36 @@ namespace Videofy.Models
 {
     public class Order
     {
+        [BindNever]
         public int OrderId { get; set; }
         public List<OrderDetail> OrderLines { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter your first name")]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your last name")]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",
+            ErrorMessage = "The email address is not entered in a correct format")]
+        public string Email { get; set; }
+
+        [BindNever]
+        [Range(1, 100)] //price must be between $1 and $100
+        [DataType(DataType.Currency)] //Format input to currency data type
+        [Column(TypeName = "decimal(18, 2)")] //Round decimal to 2 places
+        public decimal OrderTotal { get; set; } //  Ordertotal Price
+
+        [BindNever]
+        [DataType(DataType.Date)]       // Check input is type date.
+        public DateTime OrderPlaced { get; set; } // Date the order was placed.
     }
 }
